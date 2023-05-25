99b7078065ced513aab560b4ee322ffedd5fca06
Ryan Robertson <rrobertson@gmail.com>
2023-05-25 11:04:34 -0700

    pass input filename as cmd line arg

--- hello.cu
+++ hello.cu
@@ -1,19 +1,10 @@
 #include <stdio.h>
-#include <vector>
-#include <tuple>
 #include <cstdint>
-#include "bitmap.h"
 #include <fstream>
+#include "bitmap.h"
 
-#define BYTES_PER_PIXEL 3
-#define KERNEL_SIZE 13
-#define PROCESS_IMAGE 1
 #define COLORS_PER_PIXEL 3
 #define DEBUG 0
-
-// #undef __noinline__
-// #include <Magick++.h>
-// #define __noinline__ __attribute__((noinline))
 
 using namespace std;
 
@@ -23,7 +14,7 @@ struct pixel {
   uint32_t b;
 };
 
-__global__ 
+__global__
 void cuda_gamma(uint8_t *pSrc, uint8_t *pDst) {
   size_t row = blockIdx.x * blockDim.x + threadIdx.x;
   size_t col = blockIdx.y * blockDim.y + threadIdx.y;
@@ -31,11 +22,11 @@ void cuda_gamma(uint8_t *pSrc, uint8_t *pDst) {
   size_t currentPixel = (row * COLORS_PER_PIXEL * 512) + (col * COLORS_PER_PIXEL);
 
   pDst[currentPixel]   = uint8_t(255.0f * pow(float(pSrc[currentPixel])/255.0f,   (1.0f/2.2f)));
-  pDst[currentPixel+1] = uint8_t(255.0f * pow(float(pSrc[currentPixel+1])/255.0f, (1.0f/2.2f)));  
-  pDst[currentPixel+2] = uint8_t(255.0f * pow(float(pSrc[currentPixel+2])/255.0f, (1.0f/2.2f)));    
+  pDst[currentPixel+1] = uint8_t(255.0f * pow(float(pSrc[currentPixel+1])/255.0f, (1.0f/2.2f)));
+  pDst[currentPixel+2] = uint8_t(255.0f * pow(float(pSrc[currentPixel+2])/255.0f, (1.0f/2.2f)));
 }
 
-__global__ 
+__global__
 void cuda_degamma(uint8_t *pSrc, uint8_t *pDst) {
   size_t row = blockIdx.x * blockDim.x + threadIdx.x;
   size_t col = blockIdx.y * blockDim.y + threadIdx.y;
@@ -44,28 +35,28 @@ void cuda_degamma(uint8_t *pSrc, uint8_t *pDst) {
 
   pDst[currentPixel]   = uint8_t(255.0f * pow(float(pSrc[currentPixel])/255.0f,   2.2f));
   pDst[currentPixel+1] = uint8_t(255.0f * pow(float(pSrc[currentPixel+1])/255.0f, 2.2f));
-  pDst[currentPixel+2] = uint8_t(255.0f * pow(float(pSrc[currentPixel+2])/255.0f, 2.2f));  
+  pDst[currentPixel+2] = uint8_t(255.0f * pow(float(pSrc[currentPixel+2])/255.0f, 2.2f));
 }
 
 __global__
 void cuda_blur(uint8_t* pSrc, uint8_t* pDst, uint16_t kernelSize) {
   size_t row = blockIdx.x * blockDim.x + threadIdx.x;
   size_t col = blockIdx.y * blockDim.y + threadIdx.y;
+
+  size_t numPixelsKernel = kernelSize * kernelSize;
+  size_t startPos        = (row * COLORS_PER_PIXEL * 512) + (col * COLORS_PER_PIXEL);
+  size_t rowStride       = COLORS_PER_PIXEL * 512;
+
   pixel currentPixel;
-  uint16_t numPixelsKernel = kernelSize * kernelSize;
 
   bool borderPixel = (row < kernelSize/2       || col < kernelSize/2 ||
                       row > 511 - kernelSize/2 || col > 511 - kernelSize/2);
-
-  size_t startPos  = (row * COLORS_PER_PIXEL * 512) + (col * COLORS_PER_PIXEL);
-  size_t rowStride = COLORS_PER_PIXEL * 512;
 
   // ignore borders for now
   if (!borderPixel) {
     float rSum = 0.0f, gSum = 0.0f, bSum = 0.0f;
     float rAvg = 0.0f, gAvg = 0.0f, bAvg = 0.0f;
 
-    // populate NxN kernel, center pixel is middle of array
     // data organized in memory from bottom left, left to right, bottom to top
     // each pixel is 3 bytes, incrementing memory order: [0] = b, [1] = g, [2] = r
     for (int32_t i = -(kernelSize/2); i<=kernelSize/2; i++) {
@@ -101,23 +92,30 @@ void cuda_blur(uint8_t* pSrc, uint8_t* pDst, uint16_t kernelSize) {
 
 
 int main(int argc, char *argv[]) {
-  char imageFilename[] = "lenna.bmp";
+  char *imageFilename;
   uint8_t *pDevSrcImage = nullptr;
   uint8_t *pDevDstImage = nullptr;
-  uint8_t *pDevGammaDstImage = nullptr;    
-  uint8_t *pDevDeGammaDstImage = nullptr;      
+  uint8_t *pDevGammaDstImage = nullptr;
+  uint8_t *pDevDeGammaDstImage = nullptr;
   uint8_t *pHostDstImage = nullptr;
-  vector<tuple<uint8_t, uint8_t, uint8_t>> pixel;
   uint16_t kernelSize = 5;
+
+  assert(argc > 1);
+
+  if (argc > 1) {
+    imageFilename = argv[1];
+  }
+  printf("importing %s...\n", imageFilename);
+
+  if (argc > 2) {
+    kernelSize = stoi(argv[2]);
+    assert(kernelSize % 2);
+  }
+  printf("running with kernel size %d\n", kernelSize);
 
   // bring in bitmap
   Bitmap bitmap_h(imageFilename);
   bitmap_h.printBitmapInfo();
-
-  if (argv[1] != nullptr) {
-    kernelSize = stoi(argv[1]);
-  }
-  printf("running with kernel size %d\n", kernelSize); 
 
   size_t pMemSize;
   cudaDeviceGetLimit(&pMemSize, cudaLimitStackSize);
@@ -125,17 +123,17 @@ int main(int argc, char *argv[]) {
 
   // copy bitmap to device memory
   printf("Copying %s to device with a size of 0x%zX bytes\n", imageFilename, bitmap_h.getImageSize());
-  cudaMalloc((void**)&pDevSrcImage, bitmap_h.getImageSize() * sizeof(pDevSrcImage));
+  cudaMalloc((void **)&pDevSrcImage, bitmap_h.getImageSize() * sizeof(pDevSrcImage));
   cudaMemcpy(pDevSrcImage, bitmap_h.getStartOfImageData(), bitmap_h.getImageSize(), cudaMemcpyHostToDevice);
 
   // create destination/processed buffers
-  cudaMalloc((void**)&pDevDstImage,        bitmap_h.getImageSize() * sizeof(pDevDstImage));
-  cudaMalloc((void**)&pDevGammaDstImage,   bitmap_h.getImageSize() * sizeof(pDevGammaDstImage));  
-  cudaMalloc((void**)&pDevDeGammaDstImage, bitmap_h.getImageSize() * sizeof(pDevDeGammaDstImage));    
+  cudaMalloc((void **)&pDevDstImage,        bitmap_h.getImageSize() * sizeof(pDevDstImage));
+  cudaMalloc((void **)&pDevGammaDstImage,   bitmap_h.getImageSize() * sizeof(pDevGammaDstImage));
+  cudaMalloc((void **)&pDevDeGammaDstImage, bitmap_h.getImageSize() * sizeof(pDevDeGammaDstImage));
 
-  // call kernel
+  // call kernels
   dim3 blockSize = dim3(512,512,1);
-  cuda_degamma<<<blockSize, 1>>>(pDevSrcImage, pDevGammaDstImage);  
+  cuda_degamma<<<blockSize, 1>>>(pDevSrcImage, pDevGammaDstImage);
   cuda_blur<<<blockSize, 1>>>(pDevGammaDstImage, pDevDeGammaDstImage, kernelSize);
   cuda_gamma<<<blockSize, 1>>>(pDevDeGammaDstImage, pDevDstImage);
   cudaDeviceSynchronize();
@@ -148,7 +146,7 @@ int main(int argc, char *argv[]) {
     for (int i=0; i<32; i++) {
       printf("pixel processed %02d: %x\n", i, pHostDstImage[2500+i]);
     }
-  
+
     for (int i=0; i<32; i++) {
       printf("pixel original %02d: %x\n", i, bitmap_h.getImageBuffer()[2500+i]);
     }
@@ -164,10 +162,10 @@ int main(int argc, char *argv[]) {
 
   cudaFree(pDevSrcImage);
   cudaFree(pDevDstImage);
-  cudaFree(pDevGammaDstImage);  
-  cudaFree(pDevDeGammaDstImage);    
+  cudaFree(pDevGammaDstImage);
+  cudaFree(pDevDeGammaDstImage);
   cudaDeviceReset();
   free(pHostDstImage);
-  
+
   return 0;
 }
